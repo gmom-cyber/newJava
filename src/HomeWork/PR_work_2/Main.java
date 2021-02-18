228da3bb94e4f12dc3f87cc3c09eb4dccdb32092
gnom-note <moifoto2014@yandex.ru>
2021-02-18 19:22:56 +0300

    final 2pr

--- src/HomeWork/PR_work_2/Main.java
+++ src/HomeWork/PR_work_2/Main.java
@@ -9,16 +9,24 @@ public class Main {
     public static void main(String[] args) {
         List<Human> humans = StreamAPIUtil.getHuman();
         //Тестирование сортировки
+        System.out.println("Тестирование сортировки");
         List<Human> sortedHumanAsc = StreamAPIUtil.sortHumanByAge(humans);
         List<Human> sortedHumanDesc = StreamAPIUtil.sortHumanByAgeReversed(humans);
-        //StreamAPIUtil.printHuman(sortedHumanAsc);
-        //StreamAPIUtil.printHuman(sortedHumanDesc);
-
+        StreamAPIUtil.printHuman(sortedHumanAsc);
+        StreamAPIUtil.printHuman(sortedHumanDesc);
+        System .out.println("фильтрация по возрасту меньше, чем 20,");
         // фильтрация по возрасту меньше, чем 20,
         List<Human> AllHumanLessThen20 = StreamAPIUtil.AllHumanLessThen(humans,20);
         StreamAPIUtil.printHuman(AllHumanLessThen20);
 
-        // фильтрация по имени «содержит ‘е’», конкатенация первых букв имен.
-
+        // фильтрация по имени «содержит ‘е’»,
+        System .out.println("фильтрация по имени «содержит ‘е’»");
+        List<Human> NameWithE = AllHumanWithSomeLetter(humans);
+        StreamAPIUtil.printHuman(NameWithE);
+        //конкатенация первых букв имен.
+        System.out.println(StreamAPIUtil.firstLettersOfTheName(humans));
+    }
+    public static List<Human> AllHumanWithSomeLetter(List<Human> humans){
+        return humans.stream().filter(i -> i.getFirstName().contains("e")).collect(Collectors.toList());
     }
 }
--- src/HomeWork/PR_work_2/StreamAPIUtil.java
+++ src/HomeWork/PR_work_2/StreamAPIUtil.java
@@ -4,19 +4,27 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 public class StreamAPIUtil {
 
     public static List<Human> getHuman(){
         List<Human> result = new ArrayList<>();
-        result.add(new Human(50, "Ivan", "Ivanov", LocalDate.of(1971,2,8), 175));
-        result.add(new Human(60, "Fedor", "Alexandrov",LocalDate.of(1961,3,7), 195));
-        result.add(new Human(34, "Alexander ", "Sergeev", LocalDate.of(1987,9,24), 177));
-        result.add(new Human(71, "Petr", "Fedorov", LocalDate.of(1950,4,16), 171));
-        result.add(new Human(19, "Dima", "Fedorov", LocalDate.of(2002,11,11), 167));
-        result.add(new Human(12, "Nikita", "Petrov", LocalDate.of(2009,9,24), 151));
+        result.add(new Human(50, "Ivan", "Ivanov", LocalDate.of(1971,2,8), 85));
+        result.add(new Human(60, "Fedor", "Alexandrov",LocalDate.of(1961,3,7), 95));
+        result.add(new Human(34, "Alexander ", "Sergeev", LocalDate.of(1987,9,24), 77));
+        result.add(new Human(71, "Petr", "Fedorov", LocalDate.of(1950,4,16), 71));
+        result.add(new Human(19, "Dima", "Fedorov", LocalDate.of(2002,11,11), 67));
+        result.add(new Human(12, "Nikita", "Petrov", LocalDate.of(2009,9,24), 51));
 
+        return result;
+    }
+    public static List<String> getNameOfHuman(){
+        List<String> result = new ArrayList<>();
+        result.add("Ivan");
+        result.add("Fedor");
+        result.add("Alexander");
         return result;
     }
 
@@ -33,9 +41,16 @@ public class StreamAPIUtil {
     }
 
     public static List<Human> AllHumanLessThen(List<Human> humans, int age) {// фильтрация по возрасту меньше
-        return humans.stream().filter(i -> i.getAge() < age).collect(Collectors.toList());
+        return humans.stream().filter(i -> i.getAge() > age).collect(Collectors.toList());
     }
-
+    public static String firstLettersOfTheName(List<Human> h){
+        AtomicReference<String> abgdeyozh = new AtomicReference<>("");
+        h.stream().forEach((x) -> {
+                abgdeyozh.updateAndGet(v -> v + x.getFirstName().charAt(0));
+            }
+        );
+        return abgdeyozh.get();
+    }
 
     // Вывод в консоль
     public static void printHuman(List<Human> humans) {
